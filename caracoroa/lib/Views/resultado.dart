ed305223c48587b466f5b232d7c8e67de3daaf34
unknown <adminn@LAB3-22.bglab.local>
2023-05-18 20:38:44 -0300

    v3 Finish

--- caracoroa/lib/Views/resultado.dart
+++ caracoroa/lib/Views/resultado.dart
@@ -3,7 +3,8 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatefulWidget {
-  const Resultado(String resultado, {super.key});
+  String valor;
+  Resultado(this.valor, {super.key});
 
   @override
   State<Resultado> createState() => _ResultadoState();
@@ -13,12 +14,12 @@ class _ResultadoState extends State<Resultado> {
   @override
   Widget build(BuildContext context) {
 
-    // var caminhoImagem;
-    // if(Widget.valor == "cara"){
-    //   caminhoImagem = "imagens/moeda_cara_semfundo.png";
-    // }else{
-    //   caminhoImagem = "imagens/moeda_coroa_semfundo.png";
-    // }
+    var caminhoImagem = "";
+    if(widget.valor == "cara"){
+      caminhoImagem = "imagens/moeda_cara_semfundo.png";
+    }else{
+      caminhoImagem = "imagens/moeda_coroa_semfundo.png";
+    }
 
     return Scaffold(
       backgroundColor: Colors.black,
@@ -27,13 +28,11 @@ class _ResultadoState extends State<Resultado> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget> [
-              Image.asset("imagens/moeda_cara_semfundo.png"),
+            children: [
+              Image.asset(caminhoImagem),
               SizedBox(height: 20),
               GestureDetector(
-                onTap: () => {
-                  Navigator.pop(context)
-                },
+                onTap: () => {Navigator.pop(context)},
                 child: Image.asset("imagens/botao_voltar.png"),
               )
             ]
